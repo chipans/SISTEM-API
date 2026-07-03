4878589bdf431b4ec6b8fe20557f544526425346
elprogramador789 <https://github.com/elprogramador789/lab7-html.git>
2026-07-02 21:32:24 -0400

    fix: habilitar Swagger en Production y corregir configuracion de JWT

--- SistemApi.Api/Program.cs
+++ SistemApi.Api/Program.cs
@@ -67,11 +67,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(SistemWebCorsPolicy);
 
